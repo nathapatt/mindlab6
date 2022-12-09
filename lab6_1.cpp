7224058c26d0beb4311f985d56f990026ace4af3
Mind <nathapat.mind@gmail.com>
2022-12-09 16:12:56 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,19 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int number = 1;
+     int X=0,Y=0; 
+    while(number != 0){
+        cout << "Enter an integer: ";
+        cin >> number;
+    
+    if(number%2 == 0){
+        X = X+1;
+    }else{
+        Y = Y+1 ;
+    }
+    }
+    cout << "#Even numbers = " << X-1 << "\n";
+    cout << "#Odd numbers = " << Y;
     return 0;
 }
